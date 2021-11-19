4a2518f4eaa6fd0b7d4e94952fce77b963b20398
Yashashvi678 <81162244+Yashashvi678@users.noreply.github.com>
2021-11-19 14:43:53 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,36 @@
+function setup()
+{
+    video = createCapture(VIDEO);
+    video.size(550, 500);
+
+    canvas = createCanvas(550 , 500);
+    canvas.position(560, 150);
+
+    poseNet = ml5.poseNet(video, modelLoaded);
+
+    poseNet.on('pose', gotPoses);
+}
+
+
+
+function modelLoaded()
+{
+    console.log('PoseNet Is Inisialized');
+}
+
+
+
+function gotPoses(results)
+{
+    if(results.length > 0)
+    {
+        console.log(results);
+    }
+}
+
+
+
+function draw()
+{
+    background('#969A97');
+}
